e07952e3a0873356e5b5900be74f44d988de1f3d
Pradeep singh <singhpardeep585@gmail.com>
2023-05-20 19:54:46 +0530

    added new order routes for get by product id

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,6 +6,7 @@ import {
   getOrderList,
   getAllOrderList,
   changeOrderStatus,
+  getOrderListByProductId
 } from "../controllers/order.controller.js";
 import AuthRoles from "../utils/authRoles.js";
 
@@ -13,7 +14,8 @@ const router = express.Router();
 
 router.post("/create", isLoggedIn, createOrder);
 router.get("/user", isLoggedIn, getOrderList);
-router.get("/all", getAllOrderList);
+router.get("/all", isLoggedIn, getAllOrderList);
+router.get("/get/:productId", isLoggedIn, getOrderListByProductId);
 router.put("/cancel/:orderId", isLoggedIn, cancelOrder);
 router.put("/status/:orderId", isLoggedIn, authorize([AuthRoles.ADMIN, AuthRoles.MODERATOR]), changeOrderStatus)
 
