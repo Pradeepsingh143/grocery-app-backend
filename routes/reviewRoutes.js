adcb38156b6c7fcddbad367c1d2f69dac25481f1
Pradeep singh <singhpardeep585@gmail.com>
2023-05-20 19:56:56 +0530

    added review routes to get reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
-import { addReview } from "../controllers/review.controller.js";
+import { addReview, getReviewById } from "../controllers/review.controller.js";
 
 
 router.post("/add", isLoggedIn, addReview);
+router.get("/get/:id", getReviewById);
 
 export default router;
